e2020c2f6396c52e0913b82ba78f8deb76a80b97
crazyj <cmetal77@gmail.com>
2022-07-04 18:15:26 +0900

    초기 커밋.

---
+++ GoproFileRenameConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * 1. 매개변수로 입력 된 경로 내 mp4 파일 목록 가져오기
+ * 2. mp4 목록을 파일 생성 시간 기준으로 정렬 하기
+ * 3. 정렬 된 목록에 따라 파일명 앞에 순번 붙이기
+ * 4. 파일명 변경 하기
+ * 
+ * 5. 예외상황 처리
+ * 
+ * 
+ * 실행 매개변수
+ * 첫번째 : mp4 파일이 있는 폴더 경로
+ * 두번째 : 변경 될 파일이 있는 경우 덮어쓰기(-w), 무시하고 다음 파일로 넘어가기(-i), 중지(-s)
+ *          (기본값 = -i)
+ * 실행 매개변수가 없이 실행 되는 경우 안내문 표시
+ */
+
+using GoproFileRenameConsoleApp;
+
+var renameWorker = new RenameWorker();
+
+switch (args.Length)
+{
+    case 1:
+        if (!Directory.Exists(args[0]))
+        {
+            ShowHelp();
+            return;
+        }
+
+        DoWork();
+        break;
+    case 2:
+        if (RenameWorker.IsIn(args[1]))
+        {
+            renameWorker.SetRenameOption(args[1]);
+            DoWork();
+            break;
+        }
+        ShowHelp();
+        return;
+    default:
+        ShowHelp();
+        return;
+}
+
+void DoWork()
+{
+    var mp4Files = RenameWorker.ListUp(args[0]);
+
+    if (mp4Files != null && mp4Files.Any())
+    {
+        mp4Files.ForEach(m => Console.WriteLine(m.Preview()));
+
+        Console.WriteLine("위와 같이 파일명을 변경 하시겠습니까? (y/n)");
+        var answer = Console.ReadKey();
+
+        if (answer.Key == ConsoleKey.Y)
+        {
+            Console.WriteLine(string.Empty);
+            renameWorker.Rename(mp4Files, m => Console.WriteLine(m));
+        }
+    }
+}
+
+void ShowHelp()
+{
+    Console.WriteLine("GoproFileRenameConsoleApp [파일명] [옵션]");
+    Console.WriteLine("[옵션]");
+    Console.WriteLine("-i : 변경 할 파일이 존재 하는 경우 변경 하지 않고 다음 대상 파일로 넘어 갑니다.");
+    Console.WriteLine("-w : 변경 할 파일이 존재 하는 경우 덮어 씁니다.");
+    Console.WriteLine("-s : 변경 할 파일이 존재 하는 경우 작업을 종료 합니다.");
+}
+
+
---
+++ GoproFileRenameConsoleApp/RenameWorker.cs
@@ -0,0 +1,94 @@
+﻿using System.Globalization;
+
+namespace GoproFileRenameConsoleApp
+{
+    internal class RenameWorker
+    {
+        public enum RenameOptions
+        {
+            Overwrite,
+            Ignore,
+            Stop
+        }
+
+        public RenameOptions RenameOption { get; set; } = RenameOptions.Ignore;
+
+        public void Rename(IEnumerable<GoproFileName> goproFileNames, Action<string> msgOutCallback)
+        {
+            foreach(var g in goproFileNames)
+            {
+                Rename(g, msgOutCallback);
+            }
+        }
+
+        public void Rename(GoproFileName goproFileName, Action<string> msgOutCallback)
+        {
+            var orgFileName = goproFileName.OrgFileName;
+            var newFileName = goproFileName.NewFileName;
+
+            if (!File.Exists(orgFileName))
+            {
+                throw new FileNotFoundException(string.Format("{0} 파일이 존재하지 않습니다.", orgFileName));
+            }
+
+            if (File.Exists(newFileName))
+            {
+                switch(RenameOption)
+                {
+                    case RenameOptions.Overwrite:
+                        File.Move(orgFileName, newFileName, true);
+                        msgOutCallback?.Invoke(string.Format("변경 완료(덮어씀) : {0} -> {1}", Path.GetFileName(orgFileName), Path.GetFileName(newFileName)));
+                        break;
+                    case RenameOptions.Ignore:
+                        msgOutCallback?.Invoke(string.Format("변경 무시 : {0} -> {1}", Path.GetFileName(orgFileName), Path.GetFileName(newFileName)));
+                        break;
+                    case RenameOptions.Stop:
+                        throw new Exception(String.Format("변경 하려는 파일명 {0}이 이미 존재 하여 작업을 중지 합니다.", newFileName));
+                }
+            }
+            else
+            {
+                File.Move(orgFileName, newFileName);
+                msgOutCallback?.Invoke(string.Format("변경 완료 : {0} -> {1}", Path.GetFileName(orgFileName), Path.GetFileName(newFileName)));
+            }
+        }
+
+        public void SetRenameOption(string renameOption)
+        {
+            switch(renameOption.ToLower())
+            {
+                case "-w":
+                    RenameOption = RenameOptions.Overwrite;
+                    break;
+                case "-i":
+                    RenameOption = RenameOptions.Overwrite;
+                    break;
+                case "-s":
+                    RenameOption = RenameOptions.Overwrite;
+                    break;
+            }
+        }
+
+        public static bool IsIn(string renameOption)
+        {
+            var options = new[] { "-w", "-i", "-s" };
+
+            return options.Any(o => string.Compare(renameOption, o, true) == 0);
+        }
+
+        public static IEnumerable<GoproFileName>? ListUp(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return Directory.GetFiles(path)
+                                    .Where(f => Path.GetFileName(f).StartsWith("G", true, CultureInfo.CurrentCulture) &&
+                                                Path.GetFileNameWithoutExtension(f).Length == 8 &&
+                                                string.Compare(Path.GetExtension(f).ToLower(), ".mp4") == 0)
+                                    .Select(f => new GoproFileName(f))
+                                    .OrderBy(f => f.ToString());
+            }
+
+            return null;
+        }
+    }
+}
