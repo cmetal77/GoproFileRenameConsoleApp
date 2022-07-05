3d32b4480f63325cebbcb3970c2e69cba1a77618
crazyj <cmetal77@gmail.com>
2022-07-05 17:12:19 +0900

    .

--- GoproFileRenameConsoleApp/GoproFileName.cs
+++ GoproFileRenameConsoleApp/GoproFileName.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
-namespace GoproFileRenameConsoleApp
+﻿namespace GoproFileRenameConsoleApp
 {
     internal class GoproFileName
     {
